ce79845911ae30bb3b5d568b50f446e82f4b3249
Jacob Dawson <dawson@iastate.edu>
2020-10-26 08:24:59 -0500

    Add email flag

--- src/pingmewhen.go
+++ src/pingmewhen.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
 func main() {
+	emailPtr := flag.String("email", "", "Email to ping when command completes.")
+	flag.Parse()
+
+	if *emailPtr == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	command := strings.Join(flag.Args(), " ")
+	fmt.Printf("Email %s\nCommand %s\n", *emailPtr, command)
 }
